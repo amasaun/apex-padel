4653f2c53f5da659aa7da3530b606af1f677c3cb
Amit Masaun <amasaun@gmail.com>
2025-10-15 20:46:36 -0700

    calendar view - paid

--- src/pages/Matches.tsx
+++ src/pages/Matches.tsx
@@ -730,6 +730,9 @@ export default function Matches() {
                                   <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
                                 </svg>
                               )}
+                              {(match.total_cost || 0) > 0 && (
+                                <span className="flex-shrink-0">💵</span>
+                              )}
                               {formatTime(match.time)}
                             </div>
 
